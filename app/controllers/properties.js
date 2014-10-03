b1d1d7957826872865a6da0d70d9e6d01bc69b53
eric_estes <eric_estes@aspensquare.com>
2014-10-03 17:23:30 -0400

    Added some styling and javascript to handle the sortable images.

--- app/controllers/properties.js
+++ app/controllers/properties.js
@@ -4,6 +4,21 @@ export default Ember.ArrayController.extend({
 	actions: {
 		setSortable: function() {
 			console.log('Make images sortable...from the Properties controller!');
+
+			Ember.$('.sortable').sortable();
+			Ember.$('.handles').sortable({
+				handle: 'span'
+			});
+			Ember.$('.connected').sortable({
+				connectWith: '.connected'
+			});
+			Ember.$('.exclude').sortable({
+				items: ':not(.disabled)'
+			});
+
+			Ember.$('.sortable').sortable().bind('sortupdate', function () {
+				console.log('Sort has been updated');
+			});
 		}
 	}
 });
--- app/controllers/property.js
+++ app/controllers/property.js
@@ -12,6 +12,21 @@ export default Ember.ObjectController.extend({
 		},
 		setSortable: function() {
 			console.log('Make images sortable...from the Property controller:');
+
+			Ember.$('.sortable').sortable();
+			Ember.$('.handles').sortable({
+				handle: 'span'
+			});
+			Ember.$('.connected').sortable({
+				connectWith: '.connected'
+			});
+			Ember.$('.exclude').sortable({
+				items: ':not(.disabled)'
+			});
+
+			Ember.$('.sortable').sortable().bind('sortupdate', function () {
+				console.log('Sort has been updated');
+			});
 		}
 	}
 
